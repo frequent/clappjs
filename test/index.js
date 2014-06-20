b5d0acbad6780482563d8ede3842d5f30252989b
Sven Franck <sven.franck@nexedi.com>
2014-06-20 23:27:07 +0200

    test: added Qunit testbed with jslint

---
+++ test/index.js
@@ -0,0 +1,53 @@
+/*jslint indent: 2, node: true */
+
+"use strict";
+
+var toLint = [
+  "../lib/clappjs/clapp.js"
+];
+
+/*
+var fileList = [
+  // root
+  "index.html",
+  "index.js",
+  "license.txt",
+  "tests.html",
+  "tests.manifest",
+  // src
+  "src/hasOwnProperty.js",
+  "src/toString.js",
+  "src/isObject.js",
+  // wrapper
+  "src/wrapper/footer.js",
+  "src/wrapper/header.js",
+  // tests
+  "tests/jslint.js",
+  "tests/MYLIB.js"
+];
+
+var toLint = [
+  "src/hasOwnProperty.js",
+  "src/toString.js",
+  "src/isObject.js"
+    "../js/factory.js",
+  "../js/header.js",
+  "../js/import.js",
+  "../js/index.js",
+  "../js/navigation.js",
+  "../js/panel.js",
+  "../js/storage.js",
+  "../js/translation.js",
+  "../js/utilities.js",
+  "../js/viewer.js"
+];
+
+var toConcat = [
+  "license.txt",
+  "src/wrapper/header.js",
+  "src/hasOwnProperty.js",
+  "src/toString.js",
+  "src/isObject.js",
+  "src/wrapper/footer.js"
+];
+*/
