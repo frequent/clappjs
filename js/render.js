f6a042d94346ef150ed0f8bac3ac6799d9375d32
Sven Franck <sven.franck@nexedi.com>
2014-06-20 23:06:31 +0200

    update and cleanup

---
+++ js/render.js
@@ -0,0 +1,12 @@
+/* ====================================================================== */
+/*                                Render                                  */
+/* ====================================================================== */
+declare("render", ["utilities"], function (module_list) {
+  var render = {};
+
+  render.set = function (content_dict, url_dict) {
+    console.log(content_dict)
+  };
+
+  return render;
+});
