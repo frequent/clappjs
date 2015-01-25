1cecc4ffcddf18b842222ad65df90d506cd109ff
Sven Franck <svenfranck@yahoo.com>
2015-01-25 19:33:11 +0100

    polyfill: added documentation

--- src/clapp.polyfill.js
+++ src/clapp.polyfill.js
@@ -1,45 +1,57 @@
 /*jslint indent: 2, maxlen: 80, nomen: true, todo: true */
-/*global window, document, declare */
-(function (window, document) {
+/*global declare */
+(function () {
   "use strict";
 
   /* ====================================================================== */
-  /*                              POLYFILL                                  */
+  /*                            POLYFILL                                    */
+  /* ====================================================================== */
+
+  /* ====================================================================== */
+  /*                          Utility Functions                             */
   /* ====================================================================== */
 
   /**
    * =========================================================================
-   *               Try to save what's possible for IE8-9, Android 2.x
+   *                          EXPOSED METHODS
    * =========================================================================
    */
   declare("polyfill", [
     {"name": 'shim', "src": '../lib/es5shim/es5shim.js'}
   ], function () {
 
-    // Console-polyfill. MIT license.
-    // https://github.com/paulmillr/console-polyfill
-    // Make it safe to do console.log() always.
-    (function(con) {
+    /**
+      * @description | Console-polyfill. MIT license. Make it safe to do 
+      * @description | console.log() always
+      * @thanks      | https://github.com/paulmillr/console-polyfill
+      * @method      | shimConsoleLog
+      * @param       | {object},  my_console,  window.console
+      **/
+    function shimConsoleLog(my_console) {
       var prop, method, empty, dummy, properties, methods;
 
       empty = {};
-      dummy = function() {};
+      dummy = function () {};
       properties = 'memory'.split(',');
       methods = (
-        'assert,clear,count,debug,dir,dirxml,error,exception,'
+        'assert,clear,count,debug,dir,dirxml,error,exception,' +
         'group,groupCollapsed,groupEnd,info,log,markTimeline,profile,' +
-        'profiles,profileEnd,show,table,time,timeEnd,timeline,timelineEnd,'+
+        'profiles,profileEnd,show,table,time,timeEnd,timeline,timelineEnd,' +
         'timeStamp,trace,warn'
       ).split(',');
 
+      // NOTE: jslint requires "===" vs "="
       while (prop = properties.pop()) {
-        con[prop] = con[prop] || empty;
+        my_console[prop] = my_console[prop] || empty;
       }
       while (method = methods.pop()) {
-        con[method] = con[method] || dummy;
+        my_console[method] = my_console[method] || dummy;
       }
-    })(this.console = this.console || {}); // Using `this` for web workers.
+    }
 
+    // this module returns nothing, but triggers all applicable shims
+    // Using `this` for web workers.  
+    shimConsoleLog(this.console || {});
   });
 
 }());
