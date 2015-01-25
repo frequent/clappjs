a0333ee0312e04179772c8ef3ff71bb8ce48c36d
Sven Franck <svenfranck@yahoo.com>
2015-01-25 18:52:18 +0100

    hasher: jslint and documentation cleanup

--- src/clapp.hasher.js
+++ src/clapp.hasher.js
@@ -1,6 +1,6 @@
 /*jslint indent: 2, maxlen: 80, nomen: true, todo: true */
-/*global window, document, declare */
-(function (window, document) {
+/*global window, declare */
+(function (window, declare) {
   "use strict";
 
   /* ====================================================================== */
@@ -14,37 +14,38 @@
   var bracket_regex;
 
   /**
-   * Regex to extract {...} from string
-   * @property  {Regex}  bracket_regex
-   */
-  bracket_regex = /[^{}]+(?=\})/g;
+    * @description | Regex to extract {...} from string
+    * @property    | {regex},  bracket_regex
+    **/
+  // TODO: find better /[^{}]+(?=\})/g; /([^{}]+(?=\}))/g;
+  bracket_regex = /([^{}]+(?=\}))/g;
 
   /**
-   * Decode URI string
-   * @method    decode
-   * @param     {string}  my_string   String to decode
-   * @returns   {string}  encoded uri string
-   */
+    * @description | Decode URI string
+    * @method      | decode
+    * @param       | {string}, my_string, String to decode
+    * @returns     | {string}, encoded uri string
+    **/
   function decode(my_string) {
     return window.decodeURIComponent(my_string);
   }
 
   /**
-   * Encode URI string
-   * @method    encode
-   * @param     {string}  my_string   String to encode
-   * @returns   {string}  encoded uri string
-   */
+    * @description | Encode URI string
+    * @method      | encode
+    * @param       | {string}, my_string, String to encode
+    * @returns     | {string}, encoded uri string
+    **/
   function encode(my_string) {
     return window.encodeURIComponent(my_string);
   }
 
   /**
-   * Convert url key string into object
-   * @method  parseKeyString
-   * @param   {String}  my_urn    url needing to parse
-   * @returns {Object}  URN object
-   */
+    * @description | Convert url key string into object
+    * @method      | parseKeyString
+    * @param       | {string}, my_urn, url needing to parse
+    * @returns     | {object}, parsed urn object
+    **/
   // TODO: decode decode decode?
   function parseKeyString(my_key_string) {
     var url_param_list, param_list, i, len, url_dict, key;
@@ -74,11 +75,11 @@
   }
 
   /**
-    * Parse a urn hashtag into an array of key-based objects
-    * @method  parseUrn
-    * @param   {Object}  my_urn    urn to parse
-    * @returns {Array}   list of key-based objects
-    */
+    * @description | Parse a urn hashtag into an array of key-based objects
+    * @method      | parseUrn
+    * @param       | {object}, my_urn, urn to parse
+    * @returns     | {array}, list of key-based objects
+    **/
   // TODO: what was this for again???
   function parseUrn(my_urn) {
     var key_list, i, len, url_list;
@@ -95,12 +96,11 @@
   }
 
   /**
-    * Serialize urn fragment
-    * @method  serializeKeyString
-    * @param   {Object}  my_urn_dict    urn object to convert to URL
-    * @param   {Object}   my_utilities_dict
-    * @returns {String}  URL string component
-    */
+    * @description | Serialize urn fragment
+    * @method      | serializeKeyString
+    * @param       | {object}, my_urn_dict, urn object to convert to URL
+    * @param       | {object}, my_utilities_dict, Utilities object
+    * @returns     | {string}, URL string component
   function serializeKeyString(my_urn_dict, my_utilities_dict) {
     var hash_param_list, key;
 
@@ -118,7 +118,7 @@
         return param_list;
       }
       return [encode(my_key) + "=" + encode(my_val)];
-    };
+    }
 
     hash_param_list = [];
     for (key in my_urn_dict) {
@@ -128,14 +128,14 @@
     }
     return hash_param_list.join("&");
   }
+  **/
 
   /**
-    * Serialize a urn object into an bookmarkable URL
-    * @method  serializeUrn
-    * @param   {Object}   my_urn_list    urn object to convert to URL
-    * @param   {Object}   my_utilities_dict
-    * @returns {String}   URL to lookup
-    */
+    * @description | Serialize a urn object into an bookmarkable URL
+    * @method      | serializeUrn
+    * @param       | {object}, my_urn_list, urn object to convert to URL
+    * @param       | {object}, my_utilities_dict
+    * @returns     | {string}, URL to lookup
   function serializeUrn(my_urn_list, my_utilities_dict) {
     var i, len, str, key_list;
 
@@ -146,49 +146,47 @@
     }
     return str + key_list.join(encode("&"));
   }
+  **/
 
   /**
-   * Resolve link with known parameters before passing it back
-   * @method resolveLink
-   * @param   {String}  my_mangled_urn   urn string to resolve
-   * @param   {Object}  my_hate_response full response object
-   * @returns {String}  resolved link
-   */
-  function resolveLink(my_uri, my_hate_response) {
-    var bracket_list, i, len, bracket, uri;
-
-    /* prelim API:
-     * {base_url}    => location
-     *  /
-     *  {#seo}          => always set to ! for SEO crawlers
-     *  {&(...)*}       => [custom], allowing multiple RFC6570 expressions!!!
-     *  {&key}          => portal type
-     *  {&view}         => default|action[dialog]|export[dialog]
-     *  {&handler}      => method to call [all|get|put|del|new]
-     *  {&query}        => allDocs storage query
-     *  {&limit*}       => allDocs record limit from-to
-     *  {&select_list*} => allDocs fields to return
-     *  {&wildcard}     => allDocs wildcard
-     *  {&sort_on*+}    => [custom], allDocs sorting, will be join("+")ed
-     *  {&pointer}      => storage location
-     *  {&relay}        => traversal base id
-     *  {&id}           => get/put/remove id
-     */
+    * @description | Resolve link with known parameters before passing it back
+    * @description | preliminary API:
+    * @description |  {base_url}      => location
+    * @description |  {#seo}          => always set to ! for SEO crawlers
+    * @description |  {&(...)*}       => [custom], allowing multiple RFC6570?!?
+    * @description |  {&key}          => portal type
+    * @description |  {&view}         => default|action[dialog]|export[dialog]
+    * @description |  {&handler}      => method to call [all|get|put|del|new]
+    * @description |  {&query}        => allDocs storage query
+    * @description |  {&limit*}       => allDocs record limit from-to
+    * @description |  {&select_list*} => allDocs fields to return
+    * @description |  {&wildcard}     => allDocs wildcard
+    * @description |  {&sort_on*+}    => [custom], sorting, will be join("+")ed
+    * @description |  {&pointer}      => storage location
+    * @description |  {&relay}        => traversal base id
+    * @description |  {&id}           => get/put/remove id
+    * @method      | resolveLink
+    * @param       | {string}, my_mangled_urn, urn string to resolve
+    * @param       | {object}, my_hate_response, full response object
+    * @returns     | {string}, resolved link
+    **/
+  function resolveLink(my_uri) {
+    var bracket_list, i, len, bracket;
 
     bracket_list = my_uri.match(bracket_regex);
 
     for (i = 0, len = bracket_list.length; i < len; i += 1) {
       bracket = bracket_list[i];
       switch (bracket) {
-        case 'base_url':
-          my_uri = my_uri.replace('{base_url}', window.location.href);
-          break;
-        default:
-          my_uri = my_uri.replace(
-            '{' + bracket + '}',
-            '&' + encode(bracket.split(':').join('='))
-          );
-          break;
+      case 'base_url':
+        my_uri = my_uri.replace('{base_url}', window.location.href);
+        break;
+      default:
+        my_uri = my_uri.replace(
+          '{' + bracket + '}',
+          '&' + encode(bracket.split(':').join('='))
+        );
+        break;
       }
     }
 
@@ -197,25 +195,14 @@
   }
 
   /**
-   * Convert all links in a hate response to bookmarkable links
-   * @method  resolveHateResponse
-   * @param   {Object}  my_hate_response  Generated hate response object
-   * @param   {Object}  my_utilities_dict
-   * @returns {Object}  same object with all links resolved
-   */
+    * @description | Convert all links in a hate response to bookmarkable links
+    * @method      | resolveHateResponse
+    * @param       | {object}, my_hate_response, Generated hate response object
+    * @param       | {object}, my_utilities_dict
+    * @returns     | {object}, same object with all links resolved
+    **/
   function resolveHateResponse(my_hate_response, my_utilities_dict) {
-
-    // strings will be skipped here, links resolved in resolveObject
-    function testType(my_element) {
-      switch (my_utilities_dict.typeOf(my_element)) {
-        case '[object Array]':
-          resolveArray(my_element);
-          break;
-        case '[object Object]':
-          resolveObject(my_element);
-          break;
-      }
-    }
+    var testType;
 
     function resolveArray(my_list) {
       var i, len;
@@ -239,6 +226,18 @@
       }
     }
 
+    // jslint... strings will be skipped here, links resolved in resolveObject
+    testType = function (my_element) {
+      switch (my_utilities_dict.typeOf(my_element)) {
+      case '[object Array]':
+        resolveArray(my_element);
+        break;
+      case '[object Object]':
+        resolveObject(my_element);
+        break;
+      }
+    };
+
     // recursively loop over generated hate response to convert links
     testType(my_hate_response);
 
@@ -246,12 +245,12 @@
   }
 
   /**
-   * generate default hateoas response
-   * @method  createBaseHateResponse
-   * @returns new HATEOAS response object
-   */
+    * @description | Generate default hateoas response
+    * @method      | createBaseHateResponse
+    * @returns     | new HATEOAS response object
+    **/
   function createBaseHateResponse() {
-    function hate() {
+    function Hate() {
       this._embedded = {
         "contents": []
       };
@@ -272,8 +271,8 @@
       this.title = "Restricted Hateoas Polyfill";
     }
 
-    return new hate();
-  };
+    return new Hate();
+  }
 
   /**
    * =========================================================================
@@ -291,22 +290,23 @@
     "name": 'uri',
     "src": '../../lib/uri/uri.js',
     "shim": true
-  }], function (util, temp, my_uri) {
+  }], function (util) {
     var hasher = {};
 
     /**
-     * map the different http responses and everything returned from
-     * fallback queries into a homogenous format. The following response
-     * formats are handle:
-     * - fallback loading (multiple/single records): [[{},{}], [{},{}]
-     * - allDocs (select_list/full doc {data.rows[doc]}, {data.rows[value]}
-     * - get {data}
-     * @method    mapContent
-     * @param     {Object}    my_http_response    response 
-     * @returns   {Object}    formatted object
-     */
+      * @description | map the different http responses and everything returned
+      * @description | from fallback queries into a homogenous format. The
+      * @description | following response formats are handle:
+      * @description | - fallback loading (multiple/single): [[{},{}], [{},{}]
+      * @description | - allDocs {data.rows[doc]}, {data.rows[value]}
+      * @description | - get {data}
+      * @method      | mapContent
+      * @param       | {object}, my_http_response, response
+      * @returns     | {object}, formatted object
+      **/
+    // TODO: digesting is the same as above, try to merge
     hasher.mapContent = function (my_http_response) {
-      var content = [];
+      var digestResponse, content = [];
 
       // TODO: fallback data will not have ids on initial response, but ids
       // will not be used anyway at this point, so for now ok.
@@ -328,7 +328,8 @@
         }
       }
 
-      function digestResponse(my_response) {
+      // jslint...
+      digestResponse = function (my_response) {
         switch (util.typeOf(my_response)) {
         case "[object Array]":
           digestArray(my_response);
@@ -337,7 +338,7 @@
           digestObject(my_response.value || my_response.doc || my_response);
           break;
         }
-      }
+      };
 
       // start
       digestResponse(my_http_response);
@@ -346,12 +347,13 @@
     };
 
     /**
-     * retrieve a query parameter from the url looking through all key dicts
-     * @method  getUrlQueryParam
-     * @param   {String}    my_param  Parameter to retrieve
-     * @param   {String}    my_urn    Urn to parse instead of location
-     * @returns {String}    value of parameter or undefined
-     */
+      * @description | retrieve a query parameter from the url looking through
+      * @description | all key dicts
+      * @method      | getUrlQueryParam
+      * @param       | {string}, my_param, Parameter to retrieve
+      * @param       | {string}, my_urn, Urn to parse instead of location
+      * @returns     | {string}, value of parameter or undefined
+      **/
     hasher.getUrlQueryParam = function (my_param, my_urn) {
       var i, len, key_list, item, hash;
 
@@ -361,18 +363,18 @@
       for (i = 0, len = key_list.length; i < len; i += 1) {
         item = key_list[i][my_param];
         if (item) {
-          return item; 
+          return item;
         }
       }
     };
 
     /**
-     * test if a http response is "hate". If not, shim it accordingly.
-     * @method    convertResponseToHate
-     * @param     {Object}    my_http_response  xhr response object
-     * @param     {Object}    my_param_dict     request parameters
-     * @returns   {Object}    hate response
-     */
+      * @description | test if a http response is "hate". If not, shim it
+      * @method      | convertResponseToHate
+      * @param       | {object}, my_http_response, xhr response object
+      * @param       | {object}, my_param_dict, request parameters
+      * @returns     | {object} hate response
+      **/
     hasher.convertResponseToHate = function (my_http_response, my_param_dict) {
       var response, hate_dict, param_dict, param;
 
@@ -407,4 +409,4 @@
 
   });
 
-}(window, document));
+}(window, declare));
